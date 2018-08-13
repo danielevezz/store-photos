2f0c638b95a03bad9017db1ef534fc669c181f55
danielevezz <danielevezz@gmail.com>
2018-08-13 08:56:09 +0200

    polish code

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import datetime
 from PIL import Image, ExifTags
 from sys import argv
-import pprint
 from geopy.geocoders import Photon
 from geopy.point import Point
+
 
 def convert_to_degrees(value):
     d0 = value[0][0]
@@ -21,6 +21,8 @@ def convert_to_degrees(value):
 
     return d + (m / 60.0) + (s / 3600.0)
 
+
+paese = ""
 PATH = argv[1]
 
 os.chdir(PATH)
@@ -29,44 +31,48 @@ imageList = os.listdir()
 for img in imageList:
     if os.path.isdir(img):
         continue
-    imgPath = PATH + str(img) 
+    imgPath = PATH + str(img)
     if ".png" in imgPath:
         continue
 
     tmp = Image.open(imgPath)
     tags = tmp._getexif()
     if tags is not None:
-        exif = { ExifTags.TAGS[k]: v for k, v in tags.items() if k in ExifTags.TAGS }
-        #print(exif)
+        exif = {ExifTags.TAGS[k]: v for k,
+                v in tags.items() if k in ExifTags.TAGS}
+        # print(exif)
+
+        if "GPSInfo" in exif:
+            gpsinfo = {}
+
+            for key in exif['GPSInfo'].keys():
+                decode = ExifTags.GPSTAGS.get(key, key)
+                gpsinfo[decode] = exif['GPSInfo'][key]
+
+            if all(k in gpsinfo for k in ("GPSLatitude", "GPSLongitude")):
+                lat = str(convert_to_degrees(gpsinfo["GPSLatitude"]))
+                lon = str(convert_to_degrees(gpsinfo["GPSLongitude"]))
+                geolocator = Photon()
+                location = geolocator.reverse(Point(lat, lon))
+                altitude = location.altitude
+                location = location.address.split(",")
+                paese = f" - {location[5].strip()}"
+                print(paese, altitude, sep=" ")
+
         if "DateTime" in exif:
             photoDate = exif["DateTime"]
-            shotDate = datetime.datetime.strptime(photoDate, "%Y:%m:%d %H:%M:%S")
+            shotDate = datetime.datetime.strptime(
+                photoDate, "%Y:%m:%d %H:%M:%S")
             folderName = f"{shotDate:%d-%m-%Y}"
             if os.path.isdir(folderName):
                 print("Folder already present")
             else:
                 os.mkdir(folderName)
-            #os.rename(imgPath, PATH + folderName + "/" + str(img))
+            imgTmp = str(img).split(".")
+            imgName = f"{imgTmp[0]}{paese}.{imgTmp[1]}"
+            os.rename(imgPath, PATH + folderName + "/" + imgName)
+            paese = ""
         if "Model" in exif:
             print(exif["Model"])
         if "Make" in exif:
             print(exif["Make"])
-        if "GPSInfo" in exif:
-            pp = pprint.PrettyPrinter()
-            gpsinfo = {}
-
-            for key in exif['GPSInfo'].keys():
-                decode = ExifTags.GPSTAGS.get(key,key)
-                gpsinfo[decode] = exif['GPSInfo'][key]
-
-            lat = str(convert_to_degrees(gpsinfo["GPSLatitude"]))
-            lon = str(convert_to_degrees(gpsinfo["GPSLongitude"]))
-            geolocator = Photon()
-            location = geolocator.reverse(Point(lat,lon))
-            altitude = location.altitude
-            location = location.address.split(",")
-            paese = location[5].strip()
-            print(paese, altitude, sep=" ")
-
-
-
